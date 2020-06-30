0a26c1a29a24a61839d337c901da3691d1e791a5
Senatova93 <nanniin160@gmail.com>
2020-06-30 23:35:20 +0300

    grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div className='app-wrapper'>
+    <header className='header'>
+        <img src = 'https://i.pinimg.com/originals/36/5c/a2/365ca272e06580fbfc49fca845835825.jpg'/>
+
+    </header>
+     <nav className='nav'>
+         <div><a>Profile</a></div>
+         <div><a>Messages</a></div>
+         <div><a>News</a></div>
+         <div><a>Musics</a></div>
+         <div>Settings</div>
+
+     </nav>
+     <div className='content'>
+         <img src ='https://klike.net/uploads/posts/2019-06/1561182999_17.jpg'/>
+     </div>
+     <div>ava + discription</div>
+     <div>my posts
+     <div>new posts</div>
+     <div>post1</div>
+     <div>post2</div></div>
+
     </div>
   );
 }
