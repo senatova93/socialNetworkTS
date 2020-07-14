9b89b6a8bb6a527c8a21fd65e9e6599e13ab2df8
Senatova93 <nanniin160@gmail.com>
2020-07-14 06:58:41 +0300

    Dialogs

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,30 +1,46 @@
 import React from 'react'
 import s from './Dialogs.module.css'
-import { NavLink } from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
+
+type DialogType = {
+    name: string,
+    id: number
+}
+type MessageType = {
+    message: string,
+    id: number
+}
+type AnswerType = {
+    name: string
+    answer: string,
+    id: number
+}
+const Dialog = (props: DialogType) => {
+    return <div className={`${s.dialog} ${s.active}`}>
+        <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+    </div>
+}
+const Message = (props: MessageType) => {
+    return <div className={s.message}> {props.message}</div>
+}
+const Answer = (props: AnswerType) => {
+    return <div className={s.answer}>
+        <button>{props.answer} to {props.name}</button>
+    </div>
+}
 
 export function Dialogs() {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
-                <div className={`${s.dialog} ${s.active}`}>
-                    <NavLink to={'/dialogs/1'}>Nanni</NavLink>
-
-
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/2'>Ari-nari</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/3'>Misha</NavLink>
-                </div>
-
-
+                <Dialog name={"Nanni"} id={1}/>
+                <Dialog name={"Ari-Nari"} id={2}/>
+                <Dialog name={"Misha"} id={3}/>
             </div>
             <div className={s.messages}>
-                <div className={s.message}> Hi, how are you</div>
-                <div className={s.message}> Good day)</div>
-                <div className={s.message}> I have panic attack</div>
-
+                <Message message={'Hi, how are you??? '} id={1}/>
+                <Message message={'Good day'} id={1}/>
+                <Message message={'I have panic attack'} id={1}/>
             </div>
             {/*<div className={s.textareas}>*/}
             {/*    <div className={s.textarea}> <textarea></textarea> </div>*/}
@@ -34,17 +50,10 @@ export function Dialogs() {
             {/*</div>*/}
 
             <div className={s.answers}>
-                <div className={s.answer}>
-                    <button>Answer</button>
-                </div>
-                <div className={s.answer}>
-                    <button>Answer</button>
-                </div>
-                <div className={s.answer}>
-                    <button>Answer</button>
-                </div>
+                <Answer name={'Nanni'} answer={'Answer'} id={1}/>
+                <Answer name={"Ari-Nari"} answer={'Answer'} id={2}/>
+                <Answer name={"Misha"} answer={'Answer'} id={3}/>
             </div>
-
         </div>)
 
 }
